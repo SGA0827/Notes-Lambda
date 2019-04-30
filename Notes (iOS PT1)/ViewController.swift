607d786a3ed3c615c1cf2dc2bc0632c512066411
Sean Aldridge <aldridgesg@gmail.com>
2019-04-29 21:42:47 -0400

    Initial Commit

---
+++ Notes (iOS PT1)/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Notes (iOS PT1)
+//
+//  Created by Sean Aldridge on 4/29/19.
+//  Copyright © 2019 Sean Aldridge. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
